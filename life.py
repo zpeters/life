051daab9c9555c7312bd747d1351d11c09cf4f00
Zach Peters <zpeters@gmail.com>
2010-01-23 13:17:12 -0600

    Replaced height checking with static variables

--- life.py
+++ life.py
@@ -5,15 +5,13 @@ import time
 
 LIVECHAR = "+"
 DEADCHAR = " "
-LIFECHANCE = 9
+LIFECHANCE = 10
 UPDATESPEED = 0.1
-GRIDWIDTH = 39 
-GRIDHEIGHT = 39 
+GRIDWIDTH = 20
+GRIDHEIGHT = 10
 
 def gridPrinter(grid):
-    height = len(grid)
-    width = len(grid[0])
-    print "+" + "-"*height + "+"
+    print "+" + "-"*GRIDHEIGHT + "+"
     for line in grid:
         sys.stdout.write("|")
         for cell in line:
@@ -22,13 +20,13 @@ def gridPrinter(grid):
             elif cell == 1:
                 sys.stdout.write(LIVECHAR)
         sys.stdout.write("|\n")
-    print "+" + "-"*height + "+"
+    print "+" + "-"*GRIDHEIGHT + "+"
     
-def initBoard(width, height):
+def initBoard():
     grid = []
-    for x in range(0,width):
+    for x in range(0,GRIDWIDTH):
         line = []
-        for y in range(0,height):
+        for y in range(0,GRIDHEIGHT):
             spawn = random.random() * 100
             if spawn <= LIFECHANCE:
                 line.append(1)
@@ -38,57 +36,62 @@ def initBoard(width, height):
     return grid
 
 def checkCell(grid, x, y):
-    height = len(grid)
-    width = len(grid[0])
+    print "Grid is %sx%s" % (GRIDWIDTH, GRIDHEIGHT)
+    print "Checking cell [%s][%s] = %s" % (x,y, grid[x][y])
     state = grid[x][y]
 
-    if (x > 0 and x < width-1) and (y > 0 and y < height-1):
+    print "Checking bounds..."
+    if (x > 0 and x < GRIDWIDTH-1) and (y > 0 and y < GRIDWIDTH -1):
+        print "INside if statemetn"
         topNeighbors = grid[x-1][y-1] + grid[x-1][y] + grid[x-1][y+1]
         sameNeighbors = grid[x][y-1] + grid[x][y+1]
         bottomNeighbors = grid[x+1][y-1] + grid[x+1][y] + grid[x+1][y+1]
         neighbors = topNeighbors + sameNeighbors + bottomNeighbors
 
         if state == 1 and neighbors < 2:
+            print "Not enough neighbors, dying"
             newState = 0
             return newState
 
         if state == 1 and neighbors > 3:
+            print "Too many neighbors, dying"
             newState = 0
             return newState
 
         if state == 1 and neighbors == 2:
+            print "Maintaining equilibruim"
             newState = 1
             return newState
 
         if state == 1 and neighbors == 3:
+            print "Maintaining equilibruim"
             newState = 1
             return newState
 
         if state == 0 and neighbors == 3:
+            print "Neighbors just spawned me"
             newState = 1
             return newState
 
     return state
 
 def updateGrid(grid):
-    height = len(grid)
-    width = len(grid[0])
     newGrid = []
     
-    for x in range(0,height):
+    for x in range(0,GRIDHEIGHT):
         newLine = []
-        for y in range(0, width):
+        for y in range(0, GRIDWIDTH):
             newLine.append(checkCell(grid, x, y))
         newGrid.append(newLine)
 
     return newGrid
     
 def main():
-    while 1:
-        grid = initBoard(GRIDWIDTH,GRIDHEIGHT)
-        grid = updateGrid(grid)
-        gridPrinter(grid)
-        time.sleep(UPDATESPEED)
+   # while 1:
+   grid = initBoard()
+   grid = updateGrid(grid)
+   gridPrinter(grid)
+    # time.sleep(UPDATESPEED)
 
 if __name__ == "__main__":
     main()
